fe3412c4670d6df94f41d8254c8b9f87863a0b3d
shuharib0t <shuharib0t@gmail.com>
2023-09-22 18:46:48 -0300

    added admin validation in routes newdish/editdish and added Dishes/Favorites providers from src/hooks

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,5 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
+import { useAuth } from "../hooks/auth";
+import { DishesProvider } from "../hooks/dishes";
+import { FavoritesProvider } from "../hooks/favorites";
+
 import { Home } from "../pages/Home";
 import { Details } from "../pages/Details";
 import { NewDish } from "../pages/NewDish";
@@ -7,13 +11,19 @@ import { EditDish } from "../pages/EditDish";
 import { Cart } from "../pages/Cart";
 
 export function AppRoutes() {
+  const { isAdmin } = useAuth();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:id" element={<Details />} />
-      <Route path="/newdish" element={<NewDish />} />
-      <Route path="/editdish/:id" element={<EditDish />} />
-      <Route path="/shoppingcart" element={<Cart />} />
-    </Routes>
+    <DishesProvider>
+      <FavoritesProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
+          {isAdmin && <Route path="/newdish" element={<NewDish />} />}
+          {isAdmin && <Route path="/editdish/:id" element={<EditDish />} />}
+          <Route path="/shoppingcart" element={<Cart />} />
+        </Routes>
+      </FavoritesProvider>
+    </DishesProvider>
   );
 }
