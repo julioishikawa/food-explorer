c94bc5f322d2381fb37896e7c70962f5f666fc1d
shuharib0t <japaishikawa@gmail.com>
2023-10-24 21:37:29 -0300

    updating api server

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api-food-explorer-h35a.onrender.com",
+  baseURL: "https://api-food-explorer-sp4c.onrender.com",
 });
