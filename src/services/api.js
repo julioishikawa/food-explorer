26a0db08b5482d728ce485d9e4d99a90ddf60ef4
Julio Ishikawa <japaishikawa@gmail.com>
2023-11-09 23:02:38 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api-food-explorer-sp4c.onrender.com",
+  baseURL: "https://api-food-explorer-qvlv.onrender.com",
 });
